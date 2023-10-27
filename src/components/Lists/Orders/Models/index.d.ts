7586fab9d8acce8a4593cf0103f844cabf6963a4
natan.barioni <natan@carflix.com.br>
2023-10-26 22:35:01 -0300

    refactor: add where uid in get orders and add models

---
+++ src/components/Lists/Orders/Models/index.d.ts
@@ -0,0 +1,3 @@
+export interface PropsOrders {
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+}
--- src/components/Lists/Orders/index.tsx
+++ src/components/Lists/Orders/index.tsx
@@ -1,37 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 
+import auth from "@react-native-firebase/auth";
+
 import { Load } from "@components/Animations/Load";
 import { Filters } from "@components/Controllers/Filters";
-import { Order, OrderProps } from "@components/Controllers/Order";
+import { Order } from "@components/Controllers/Order";
 import { Container, Header, Title, Counter } from "./styles";
 
 import firestore from "@react-native-firebase/firestore";
+import { OrderProps } from "@components/Controllers/Order/Models";
+import { PropsOrders } from "./Models";
 
-export function Orders() {
+export function Orders({setIsOpen}: PropsOrders) {
   const [status, setStatus] = useState("open");
   const [isLoading, setIsLoading] = useState(false);
   const [orders, setOrders] = useState<OrderProps[]>([]);
 
   useEffect(() => {
     setIsLoading(true);
+    const user = auth().currentUser;
+    if (user) {
+      const subscriber = firestore()
+        .collection("orders")
+        .where("status", "==", status)
+        .where('uid', '==', user?.uid)
+        .onSnapshot((querySnapshot) => {
+          const data = querySnapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          }) as OrderProps[];
 
-    const subscriber = firestore()
-      .collection("orders")
-      .where('status', '==', status)
-      .onSnapshot((querySnapshot) => {
-        const data = querySnapshot.docs.map((doc) => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          };
-        }) as OrderProps[];
+          setOrders(data);
+          setIsLoading(false);
+        });
 
-        setOrders(data);
-        setIsLoading(false);
-      });
-
-    return () => subscriber();
+      return () => subscriber();
+    }
   }, [status]);
 
   return (
@@ -49,7 +56,7 @@ export function Orders() {
         <FlatList
           data={orders}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Order data={item} />}
+          renderItem={({ item }) => <Order data={item} setIsOpen={setIsOpen}/>}
           contentContainerStyle={{ paddingBottom: 100 }}
           showsVerticalScrollIndicator={false}
           style={{ flex: 1 }}
