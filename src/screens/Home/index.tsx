30e63780fc65070932a90c266caf9c71f310cf16
natan.barioni <natan@carflix.com.br>
2023-10-26 22:35:24 -0300

    feat: add isOpen to open bottomsheet

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Container } from './styles';
-import { Header } from '@components/Layout/Header';
-import { Orders } from '@components/Lists/Orders';
-import { NewOrder } from '@components/Controllers/NewOrder';
+import { Container } from "./styles";
+import { Header } from "@components/Layout/Header";
+import { Orders } from "@components/Lists/Orders";
+import { NewOrder } from "@components/Controllers/NewOrder";
 
 export function Home() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Container>
       <Header />
-      <Orders />
-      <NewOrder />
+      <Orders setIsOpen={setIsOpen}/>
+      <NewOrder isOpen={isOpen} setIsOpen={setIsOpen} />
     </Container>
   );
 }
